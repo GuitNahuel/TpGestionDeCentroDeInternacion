4b0249782d51d0198e93e80cc1f97fbd9663cbe7
Nahuel Guitian <109321382+GuitNahuel@users.noreply.github.com>
2024-05-27 23:06:14 -0300

    Comienzo de proyecto

---
+++ test/unlam/ar/testing/TestingDelCentroDeInternacion.java
@@ -0,0 +1,15 @@
+package unlam.ar.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestingDelCentroDeInternacion {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+		//Comentario
+	}
+
+}
